9dde92411038c4e3df9b674fbb6322524e7f7276
Hansonair <126552528+Hansonair@users.noreply.github.com>
2023-03-10 17:59:59 +1030

    Update question08.sh

--- question08.sh
+++ question08.sh
@@ -1,7 +1,14 @@
+git init
 touch file.txt
+touch 3.txt
 touch file.odf
 mkdir dir2
 
 mv *.txt dir2/
+git commit -m "Moved text files to dir2"
+nano question08.sh
+# Copy and paste the commands into the file, then save and exit.
+
+chmod +x question08.sh
 
 
