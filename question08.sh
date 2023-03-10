1899afc89d89337a398195bba0a452597e5d6f2e
Hansonair <126552528+Hansonair@users.noreply.github.com>
2023-03-10 17:31:00 +1030

    Update question08.sh

--- question08.sh
+++ question08.sh
@@ -3,3 +3,7 @@ mv *.txt dir2/
 git add dir2/
 
 git commit -m "Moved text files to dir2"
+nano question08.sh
+# Copy and paste the commands into the file, then save and exit.
+
+chmod +x question08.sh
