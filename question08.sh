6b49d7b5e80309b0fdf7bce2487edee4c16e7d08
Hansonair <126552528+Hansonair@users.noreply.github.com>
2023-03-12 14:35:59 +1030

    Update question08.sh

--- question08.sh
+++ question08.sh
@@ -1,14 +1,27 @@
+git init
 
 touch file.txt
 touch 3.txt
 
 touch file.pdf
-git init
+
+
+
+
+
+# Create a directory named dir2
 mkdir dir2
 
+# Move all text files from the current directory to dir2
 mv *.txt dir2/
-gir add dir2/
-git commit -m "Moved text files to dir2"
 
+# Stage the changes
+git add .
 
+# Commit the changes with a message
+git commit -m "Move all text files to dir2"
 
+# Verify the changes
+ls
+ls dir2/
+git log
