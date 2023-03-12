ee2387a47fb63e56fe8bda99d82ae5e6cd59f255
Hansonair <126552528+Hansonair@users.noreply.github.com>
2023-03-12 14:39:16 +1030

    Update question08.sh

--- question08.sh
+++ question08.sh
@@ -9,19 +9,18 @@ touch file.pdf
 
 
 
-# Create a directory named dir2
+
 mkdir dir2
 
-# Move all text files from the current directory to dir2
 mv *.txt dir2/
 
-# Stage the changes
+
 git add .
 
-# Commit the changes with a message
+
 git commit -m "Move all text files to dir2"
 
-# Verify the changes
+
 ls
 ls dir2/
 git log
