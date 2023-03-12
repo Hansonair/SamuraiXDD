7e8f9c9d6e38f917b4e43a60930d9c4562230c53
Hansonair <126552528+Hansonair@users.noreply.github.com>
2023-03-12 15:10:01 +1030

    Update question11.sh

--- question11.sh
+++ question11.sh
@@ -4,12 +4,11 @@ git checkout -b branch2
 # Create and commit a file named file4
 touch file4
 git add file4
-git commit -m "Add file4 in branch2"
+git commit -m "Add file4"
 
 # Modify file4
-echo "Some new content" > file4
+echo "Some new content" >> file4
 
 # Switch back to the main branch without committing the changes and stash the changes
 git stash
 git checkout main
-git stash apply
