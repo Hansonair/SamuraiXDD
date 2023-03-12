e1fcc0679365f8280805cee8199d322620a82c22
Hansonair <126552528+Hansonair@users.noreply.github.com>
2023-03-12 15:31:39 +1030

    Update question13.sh

--- question13.sh
+++ question13.sh
@@ -1,15 +1,15 @@
 
-
-# Create a new branch for this assignment
-git checkout -b assignment
-
 # Remove all .sh files from other questions
 find . -type f -name "*.sh" -delete
 
-# Create, add, and commit a new text file named file13.txt to that branch
-echo "This is file13" > file13.txt
+# Create a new text file named file13.txt and add some content
+echo "This is file13." > file13.txt
+
+# Add file13.txt to the staging area
 git add file13.txt
+
+# Commit the changes with a commit message
 git commit -m "Add file13.txt"
 
 # Push the new branch to GitHub
-git push --set-upstream origin assignment
+git push -u origin <branch_name>
