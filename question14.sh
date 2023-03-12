63cb3ea3b7bcdeae3202794bcfba107521bd754c
Hansonair <126552528+Hansonair@users.noreply.github.com>
2023-03-12 15:21:28 +1030

    Create question14.sh

---
+++ question14.sh
@@ -0,0 +1,11 @@
+# Clone the repository
+git clone https://github.com/ian-knight-uofa/git-practice-04.git
+
+# Change directory to the repository
+cd git-practice-04
+
+# Fetch all branches from the remote server
+git fetch origin
+
+# Switch to the update1 branch
+git checkout update1
