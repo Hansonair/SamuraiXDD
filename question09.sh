3742612f8d21ed5bae3a7a8785dce6a736ecec41
Hansonair <126552528+Hansonair@users.noreply.github.com>
2023-03-12 14:54:11 +1030

    Update question09.sh

--- question09.sh
+++ question09.sh
@@ -1,18 +1,7 @@
-# Create a branch named branch1 in the current repository
 git branch branch1
-
-# Switch to the branch1 branch
 git checkout branch1
-
-# Create a new file named file3 in the new branch
 touch file3
-
-# Stage the new file
 git add file3
-
-# Commit the changes with a message
 git commit -m "Add file3 in branch1"
-
-# Verify the changes
 git log
 
